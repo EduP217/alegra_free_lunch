f03f9c15f6deef824ebfd8799e0bbd4ae549be3e
EduP217 <prieto_217@hotmail.com>
2022-04-30 08:38:00 -0500

    last changes on services

--- services/app.js
+++ services/app.js
@@ -6,8 +6,8 @@ const cookieParser = require('cookie-parser');
 const rTracer = require('cls-rtracer');
 const path = require('path');
 global.appRoot = path.resolve(__dirname);
-//global.marketplaceURL = 'https://recruitment.alegra.com/api/farmers-market/buy';
-global.marketplaceURL = 'http://localhost:3000/buy';
+global.marketplaceURL = 'https://recruitment.alegra.com/api/farmers-market/buy';
+//global.marketplaceURL = 'http://localhost:3000/buy';
 
 const app = express();
 
@@ -45,73 +45,6 @@ app.get('/', function (req, res) {
 app.use('/app/v1/recipes', require('./routes/recipes.routes'));
 app.use('/app/v1/warehouse', require('./routes/warehouse.routes'));
 app.use('/app/v1/orders', require('./routes/orders.routes'));
-
-/*app.get('/recipes', function (req, res) {
-    let recipes = 
-    res.send(recipes);
-});
-
-app.post('/usuario', function (req, res) {
-    if (!req.body.nombre || !req.body.apellido) {
-        respuesta = {
-            error: true,
-            codigo: 502,
-            mensaje: 'El campo nombre y apellido son requeridos'
-        };
-    } else {
-        if (usuario.nombre !== '' || usuario.apellido !== '') {
-            respuesta = {
-                error: true,
-                codigo: 503,
-                mensaje: 'El usuario ya fue creado previamente'
-            };
-        } else {
-            usuario = {
-                nombre: req.body.nombre,
-                apellido: req.body.apellido
-            };
-            respuesta = {
-                error: false,
-                codigo: 200,
-                mensaje: 'Usuario creado',
-                respuesta: usuario
-            };
-        }
-    }
-
-    res.send(respuesta);
-});
-
-app.put('/usuario', function (req, res) {
-    if (!req.body.nombre || !req.body.apellido) {
-        respuesta = {
-            error: true,
-            codigo: 502,
-            mensaje: 'El campo nombre y apellido son requeridos'
-        };
-    } else {
-        if (usuario.nombre === '' || usuario.apellido === '') {
-            respuesta = {
-                error: true,
-                codigo: 501,
-                mensaje: 'El usuario no ha sido creado'
-            };
-        } else {
-            usuario = {
-                nombre: req.body.nombre,
-                apellido: req.body.apellido
-            };
-            respuesta = {
-                error: false,
-                codigo: 200,
-                mensaje: 'Usuario actualizado',
-                respuesta: usuario
-            };
-        }
-    }
-
-    res.send(respuesta);
-});*/
 
 app.get('/buy', function (req, res) {
     res.send({"quantitySold": 2});
--- services/routes/orders.routes.js
+++ services/routes/orders.routes.js
@@ -25,4 +25,88 @@ router.get('/', async (req, res) => {
     return res.status(200).json(warehouse);
 });
 
+router.post('/', async (req, res) => {
+    let recipes = await utilitaries.readFromCSV('/data/recipes.csv');
+    let recipesIngredients = await utilitaries.readFromCSV('/data/recipes-ingredients.csv');
+    let ingredients = await utilitaries.readFromCSV('/data/ingredients.csv');
+
+    let randomRecipe = recipes[Math.floor(Math.random() * recipes.length)];
+    let recipesIngredientsFiltered = utilitaries.getFilteredByKey(recipesIngredients, "recipe_id", randomRecipe.id);
+    let ingredientsWithoutStock = false;
+
+    await ingredients.map(async i => {
+        if (!ingredientsWithoutStock) {
+            await recipesIngredientsFiltered.forEach(ir => {
+                if (i.id == ir.ingredient_id) {
+                    let calculateReduceStock = i.stock - ir.quantity;
+                    if (calculateReduceStock < 0) {
+                        ingredientsWithoutStock = true;
+                    } else if (calculateReduceStock == 0) {
+                        i.stock = calculateReduceStock;
+                        i.status = 0;
+                    } else {
+                        i.stock = calculateReduceStock;
+                    }
+                }
+            });
+        }
+        return i;
+    });
+
+    let message = {};
+
+    if (ingredientsWithoutStock) {
+        message = {
+            "message": `There are not enough ingredients to prepare the <b>'${randomRecipe.name}'</b>`
+        };
+    } else {
+        console.log(ingredients);
+
+        let ingredientsOptions = [{
+                key: 'id',
+                header: 'id'
+            },
+            {
+                key: 'name',
+                header: 'name'
+            },
+            {
+                key: 'stock',
+                header: 'stock'
+            },
+            {
+                key: 'status',
+                header: 'status'
+            }
+        ];
+        utilitaries.writeCSV('/data/ingredients.csv', ingredientsOptions, ingredients);
+
+        let orders = await utilitaries.readFromCSV('/data/orders.csv');
+        let newId = `OR-${utilitaries.zeroPad(orders.length,3)}`;
+        orders.push({
+            "id": newId,
+            "recipe_id": randomRecipe.id,
+            "ordered": req.body.dateOrdered,
+            "delivered": "",
+            "status": "0"
+        });
+
+        let ordersOptions = [
+            {key: 'id',header: 'id'},
+            {key: 'recipe_id',header: 'recipe_id'},
+            {key: 'ordered',header: 'ordered'},
+            {key: 'delivered',header: 'delivered'},
+            {key: 'status',header: 'status'}
+        ];
+        utilitaries.writeCSV('/data/orders.csv', ordersOptions, orders);
+
+        message = {
+            "message": `The order <b>${newId}</b> has been sent to the kitchen.`
+        };
+    }
+
+    console.log(message);
+    return res.status(201).send(message);
+});
+
 module.exports = router;

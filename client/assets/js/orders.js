af16b56bfce7eaca30749a6738fde85bbe6c25a3
EduP217 <prieto_217@hotmail.com>
2022-04-29 20:50:26 -0500

    init order page

--- client/assets/js/orders.js
+++ client/assets/js/orders.js
@@ -1,31 +1,46 @@
-function getData() {
-    renderOrdersOnHold();
-    renderOrdersToPickUp();
-    renderOrdersHistory();
+async function init() {
+    let orders = await getData("data/orders.json");
+
+    renderOrdersOnHold(orders.pending);
+    renderOrdersToPickUp(orders["pick-up"]);
+    renderOrdersHistory(orders.history);
 }
 
-function renderOrdersOnHold() {
+function renderOrdersOnHold(list) {
     const root = document.getElementById('orders-cards-container');
     const template = document.querySelector('#orderpendingrow');
+    for (const i of list) {
+        let item = template.content.cloneNode(true);
+        let itemName = item.querySelector(".item-name");
+        let itemImage = item.querySelector(".item-image");
+        let itemTime = item.querySelector(".item-time");
 
-    let item = template.content.cloneNode(true);
-    root.appendChild(item);
+        itemName.innerHTML = i.food;
+        itemImage.setAttribute("src",`${basepath}/assets/${i.image}`);
+        itemImage.setAttribute("alt",i.food);
+
+        let itemOrdered = new Date(itemTime);
+        console.log(itemOrdered);
+        root.appendChild(item);
+    }
 }
 
-function renderOrdersToPickUp() {
+function renderOrdersToPickUp(list) {
     const root = document.getElementById('orders-list-item-container');
     const template = document.querySelector('#orderitempickup');
-
-    let item = template.content.cloneNode(true);
-    root.appendChild(item);
+    for (const i of list) {
+        let item = template.content.cloneNode(true);
+        root.appendChild(item);
+    }
 }
 
-function renderOrdersHistory() {
+function renderOrdersHistory(list) {
     const tbody = document.querySelector('#table-orders-history tbody');
     const template = document.querySelector('#orderhistoryrow');
-
-    let item = template.content.cloneNode(true);
-    tbody.appendChild(item);
+    for (const i of list) {
+        let item = template.content.cloneNode(true);
+        tbody.appendChild(item);
+    }
 }
 
-getData();
+init();
--- client/assets/js/utils.js
+++ client/assets/js/utils.js
@@ -1,4 +1,6 @@
 const basepath = 'https://edup217.github.io/alegra_free_lunch/client';
+const currentDate = new Date();
+console.log(currentDate);
 
 function getData(url) {
     return fetch(`${basepath}/${url}`).then((res) => res.json()).then((data) => data);
